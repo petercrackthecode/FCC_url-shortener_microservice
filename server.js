0551f2ea537677269ab677ba51c746dc903c4c4c
petercrackthecode <nguyenvandung02051999@gmail.com>
2021-03-30 15:21:35 -0700

    Add body parser. Learning node

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
 
-var cors = require('cors');
+require("dotenv").config();
+
+var cors = require("cors");
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
+/** this project needs a db !! **/
 // mongoose.connect(process.env.DB_URI);
 
+// process.cwd() prints out the current working directory
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res, next) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api/hello", function (req, res, next) {
+  res.json({ greeting: "hello API" });
 });
 
+app.post("/api/shorturl/new", (req, res, next) => {
+  console.log(req.body);
+  res.json({ "req.body": req.body, Hi: "hi" });
+});
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log(`Node.js listening at port ${port}`);
 });
